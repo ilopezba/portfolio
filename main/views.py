f8f46bd4bf54d5625bfcf7f3d6afaeafe002e403
ilopezba <ivanlb01@gmail.com>
2016-05-09 15:51:35 +0200

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,7 +1,3 @@
-"""
-A view is a callable which takes a request and returns a response. This can be more than just a function, and Django provides an example of some classes which can be used as views. These allow you to structure your views and reuse code by harnessing inheritance and mixins. There are also some generic views for simple tasks which we'll get to later, but you may want to design your own structure of reusable views which suits your use case.
-"""
-
 from django.http import HttpResponse
 from django.http import Http404
 from django.template import Context
